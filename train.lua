d554a2743ad03ffb82c9d1c0b303c4873817a0bd
manu <roche.emmanuel@gmail.com>
2015-12-19 23:42:20 +0100

    Working on issue #4: Adding initial version of train.lua.

---
+++ train.lua
@@ -0,0 +1,96 @@
+require 'torch'
+require 'nn'
+require 'nngraph'
+require 'optim'
+require 'lfs'
+
+local FOREXLoader = require "FOREXLoader"
+
+cmd = torch.CmdLine()
+cmd:text()
+cmd:text('Train a FOREX trading agent')
+cmd:text()
+cmd:text('Options')
+-- data
+cmd:option('-data_dir','inputs/2004_01_to_2004_04','data directory. Should contain the input data for the training')
+-- model params
+cmd:option('-rnn_size', 128, 'size of LSTM internal state')
+cmd:option('-num_layers', 2, 'number of layers in the LSTM')
+cmd:option('-model', 'lstm', 'lstm, gru or rnn')
+-- optimization
+cmd:option('-learning_rate',2e-3,'learning rate')
+cmd:option('-learning_rate_decay',0.97,'learning rate decay')
+cmd:option('-learning_rate_decay_after',10,'in number of epochs, when to start decaying the learning rate')
+cmd:option('-decay_rate',0.95,'decay rate for rmsprop')
+cmd:option('-dropout',0,'dropout for regularization, used after each RNN hidden layer. 0 = no dropout')
+cmd:option('-seq_length',50,'number of timesteps to unroll for')
+cmd:option('-batch_size',50,'number of sequences to train on in parallel')
+cmd:option('-max_epochs',50,'number of full passes through the training data')
+cmd:option('-grad_clip',5,'clip gradients at this value')
+cmd:option('-train_frac',0.95,'fraction of data that goes into train set')
+cmd:option('-val_frac',0.05,'fraction of data that goes into validation set')
+            -- test_frac will be computed as (1 - train_frac - val_frac)
+cmd:option('-init_from', '', 'initialize network parameters from checkpoint at this path')
+-- bookkeeping
+cmd:option('-seed',123,'torch manual random number generator seed')
+cmd:option('-print_every',1,'how many steps/minibatches between printing out the loss')
+cmd:option('-eval_val_every',1000,'every how many iterations should we evaluate on validation data?')
+cmd:option('-checkpoint_dir', 'cv', 'output directory where checkpoints get written')
+cmd:option('-savefile','lstm','filename to autosave the checkpont to. Will be inside checkpoint_dir/')
+cmd:option('-accurate_gpu_timing',0,'set this flag to 1 to get precise timings when using GPU. Might make code bit slower but reports accurate timings.')
+-- GPU/CPU
+cmd:option('-gpuid',0,'which gpu to use. -1 = use CPU')
+cmd:option('-opencl',0,'use OpenCL (instead of CUDA)')
+cmd:text()
+
+-- parse input params
+opt = cmd:parse(arg)
+torch.manualSeed(opt.seed)
+
+-- train / val / test split for data, in fractions
+local test_frac = math.max(0, 1 - (opt.train_frac + opt.val_frac))
+local split_sizes = {opt.train_frac, opt.val_frac, test_frac} 
+
+-- initialize cunn/cutorch for training on the GPU and fall back to CPU gracefully
+if opt.gpuid >= 0 and opt.opencl == 0 then
+  local ok, cunn = pcall(require, 'cunn')
+  local ok2, cutorch = pcall(require, 'cutorch')
+  if not ok then print('package cunn not found!') end
+  if not ok2 then print('package cutorch not found!') end
+  if ok and ok2 then
+    print('using CUDA on GPU ' .. opt.gpuid .. '...')
+    cutorch.setDevice(opt.gpuid + 1) -- note +1 to make it 0 indexed! sigh lua
+    cutorch.manualSeed(opt.seed)
+  else
+    print('If cutorch and cunn are installed, your CUDA toolkit may be improperly configured.')
+    print('Check your CUDA toolkit installation, rebuild cutorch and cunn, and try again.')
+    print('Falling back on CPU mode')
+    opt.gpuid = -1 -- overwrite user setting
+  end
+end
+
+-- initialize clnn/cltorch for training on the GPU and fall back to CPU gracefully
+if opt.gpuid >= 0 and opt.opencl == 1 then
+  local ok, cunn = pcall(require, 'clnn')
+  local ok2, cutorch = pcall(require, 'cltorch')
+  if not ok then print('package clnn not found!') end
+  if not ok2 then print('package cltorch not found!') end
+  if ok and ok2 then
+    print('using OpenCL on GPU ' .. opt.gpuid .. '...')
+    cltorch.setDevice(opt.gpuid + 1) -- note +1 to make it 0 indexed! sigh lua
+    torch.manualSeed(opt.seed)
+  else
+    print('If cltorch and clnn are installed, your OpenCL driver may be improperly configured.')
+    print('Check your OpenCL driver installation, check output of clinfo command, and try again.')
+    print('Falling back on CPU mode')
+    opt.gpuid = -1 -- overwrite user setting
+  end
+end
+
+-- make sure output directory exists
+if not path.exists(opt.checkpoint_dir) then lfs.mkdir(opt.checkpoint_dir) end
+
+-- prepare the loader:
+local loader = FOREXLoader.create(opt.data_dir, opt.batch_size, opt.seq_length, split_sizes)
+
+print("Training done.")
