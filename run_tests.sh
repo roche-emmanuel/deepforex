c4d1ec2e5d8b0462e08d508f990450a8e9a9406b
manu <roche.emmanuel@gmail.com>
2016-01-07 12:03:49 +0100

    Working on issue #19: Added support for test v43.

--- run_tests.sh
+++ run_tests.sh
@@ -394,50 +394,69 @@ source ~/scripts/profile.sh
 #   -with_timetag 1 \
 #   -with_close_only 1
 
-dforex_screen_d v42b \
-  -data_dir inputs/mt5_2015_12 \
-  -num_remas 2 \
-  -num_emas 1 \
-  -rsi_period 9 \
-  -batch_size 80 \
-  -eval_size 10 \
-  -max_sessions 1000 \
-  -max_epochs 15 \
-  -initial_max_epochs 100 \
-  -log_return_offsets 3 \
-  -forcast_index 1 \
-  -seed 124 \
-  -with_timetag 1 \
-  -with_close_only 1
+# dforex_screen_d v42b \
+#   -data_dir inputs/mt5_2015_12 \
+#   -num_remas 2 \
+#   -num_emas 1 \
+#   -rsi_period 9 \
+#   -batch_size 80 \
+#   -eval_size 10 \
+#   -max_sessions 1000 \
+#   -max_epochs 15 \
+#   -initial_max_epochs 100 \
+#   -log_return_offsets 3 \
+#   -forcast_index 1 \
+#   -seed 124 \
+#   -with_timetag 1 \
+#   -with_close_only 1
 
-dforex_screen_d v42c \
-  -data_dir inputs/mt5_2015_12 \
-  -num_remas 2 \
-  -num_emas 1 \
-  -rsi_period 9 \
-  -batch_size 80 \
-  -eval_size 10 \
-  -max_sessions 1000 \
-  -max_epochs 15 \
-  -initial_max_epochs 100 \
-  -log_return_offsets 3 \
-  -forcast_index 1 \
-  -seed 125 \
-  -with_timetag 1 \
-  -with_close_only 1
+# dforex_screen_d v42c \
+#   -data_dir inputs/mt5_2015_12 \
+#   -num_remas 2 \
+#   -num_emas 1 \
+#   -rsi_period 9 \
+#   -batch_size 80 \
+#   -eval_size 10 \
+#   -max_sessions 1000 \
+#   -max_epochs 15 \
+#   -initial_max_epochs 100 \
+#   -log_return_offsets 3 \
+#   -forcast_index 1 \
+#   -seed 125 \
+#   -with_timetag 1 \
+#   -with_close_only 1
 
-dforex_screen_d v42d \
+# dforex_screen_d v42d \
+#   -data_dir inputs/mt5_2015_12 \
+#   -num_remas 2 \
+#   -num_emas 1 \
+#   -rsi_period 9 \
+#   -batch_size 80 \
+#   -eval_size 10 \
+#   -max_sessions 1000 \
+#   -max_epochs 15 \
+#   -initial_max_epochs 100 \
+#   -log_return_offsets 3 \
+#   -forcast_index 1 \
+#   -seed 126 \
+#   -with_timetag 1 \
+#   -with_close_only 1
+
+dforex_screen v43 \
   -data_dir inputs/mt5_2015_12 \
+  -num_layers 3 \
+  -rnn_size 256 \
+  -seq_length 40 \
   -num_remas 2 \
   -num_emas 1 \
   -rsi_period 9 \
   -batch_size 80 \
   -eval_size 10 \
   -max_sessions 1000 \
-  -max_epochs 15 \
-  -initial_max_epochs 100 \
+  -max_epochs 20 \
+  -initial_max_epochs 400 \
   -log_return_offsets 3 \
   -forcast_index 1 \
-  -seed 126 \
+  -seed 123 \
   -with_timetag 1 \
   -with_close_only 1
